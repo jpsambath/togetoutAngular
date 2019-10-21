c85c787001cb078225b8d7beb20788b9e3e0fb58
jpsambath <jp@karmage.fr>
2019-10-21 16:50:49 +0200

    Début Angular JPS

---
+++ togetoutAngular/src/app/inscription-form/inscription-form.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Participant } from "../participant";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-inscription-form',
+  templateUrl: './inscription-form.component.html',
+  styleUrls: ['./inscription-form.component.css']
+})
+export class InscriptionFormComponent implements OnInit {
+
+  inscriptionForm : FormGroup;
+
+  constructor(private formBuilder: FormBuilder/*,private participantService: participantService*/, private router : Router){
+
+  }
+  ngOnInit(){
+    this.inscriptionForm = this.formBuilder.group({
+      nom : '',
+      prenom : '',
+      telephone : '',
+      mail : ''
+    });
+  }
+
+  onSubmitForm(){
+    const formValue = this.inscriptionForm.value;
+    const nouveauParticipant = new Participant(
+      formValue['nom'],
+      formValue['prenom'],
+      formValue['telephone'],
+      formValue['mail'],
+      false,
+      true,
+      null
+    );
+    //this.participantService.ajouterParticipant(nouveauParticipant);
+    this.router.navigate(['/accueil']);
+
+  }
+
+}
---
+++ togetoutAngular/src/app/participant.ts
@@ -0,0 +1,15 @@
+export class Participant {
+
+  constructor(
+
+    public nom?: string,
+    public prenom?: string,
+    public telephone?: string,
+    public mail?: string,
+    public administrateur?: boolean,
+    public actif?: boolean,
+    public id?: number
+
+  ) {  }
+
+}
