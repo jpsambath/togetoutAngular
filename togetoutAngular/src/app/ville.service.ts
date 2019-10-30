903c5c0c41fa5f52ba0a9d847cfe16d239976626
MaximeRoulin <maxime.roulin@laposte.net>
2019-10-30 18:48:59 +0100

    CORS remarche

--- togetoutAngular/src/app/ville.service.ts
+++ togetoutAngular/src/app/ville.service.ts
@@ -5,6 +5,8 @@ import { AuthService} from "./auth.service";
 import {Observable, of} from "rxjs";
 import {MessageService} from "./message.service";
 import * as MesConstantes from './model/global'
+import {Sortie} from "./model/sortie";
+import {Ville} from "./model/ville";
 
 @Injectable({
   providedIn: 'root'
@@ -53,6 +55,32 @@ export class VilleService {
       });
     })
 
+  }
+
+  public creerVille(ville: Ville){
+    return new Promise((resolve, reject) => {
+      this.header = new HttpHeaders({
+        'Content-Type':  'application/json',
+        //'Access-Control-Allow-Origin' : '*',
+        'Authorization': 'Bearer ' + this.authService.token
+      });
+
+      /* Stocker Observable dans attribut du service pour écoute par d'autres composants */
+      this.httpClient.post(MesConstantes.api+'ajoutVille', ville, { "headers" :this.header}).pipe(
+        catchError(this.handleError('ajoutVille', ville))
+      ).subscribe((data)=>{
+
+        this.resultat = data;
+
+        if (this.resultat['statut'] == 'ok') {
+          this.messageService.messageSucces = "Ville créée avec succès";
+          resolve("creerVille ok");
+        } else {
+          this.messageService.messageErreur = "Ville non créée";
+          reject("creerVille ko");
+        }
+      });
+    })
   }
 
 }
